f89bf6a722b0690ff4931a5e888bdb033f65a9ee
Sudhanshu Chandekar <schandek@gmu.edu>
2014-12-20 00:34:46 -0500

    HW2new

--- cachematrix.R
+++ cachematrix.R
@@ -7,14 +7,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse <- NULL
+  ## function to store the input numeric matrix
   set <- function(y){
     x <<- y
     inverse <<- NULL
   }
+  ## function to retrieve the input matrix
   get <- function() x
+  ## function to retrieve the inverse of the input matrix
   getinv <- function() inverse
+  ## function to cache the inverse
   setinv <- function(inv) inverse <<- inv
-  
+  ## list of functions that make the numeric matrix a special matrix
   list(set=set, get=get, setinv=setinv, getinv= getinv)
 }
 
@@ -26,12 +30,15 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inverse <- x$getinv()
+  ## Checks if the inverse exists in cache
   if (!is.null(inverse)){
     message("getting cahced data")
     return(inverse)
   }
+  ## If no inverse then compute one
   data <- x$get()
   inverse <- solve(data)
+  ##Cache the inverse of the special matrix
   x$setinv(inverse)
   inverse
 }
